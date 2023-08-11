0ee991735d812e3921a9d724cf60829a6b04e47a
chanoking <134757772+chanoking@users.noreply.github.com>
2023-08-11 13:52:36 +0900

    Add files via upload
    대략적인 css 수정 후 버전

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-application = app = Flask(__name__)
+app = Flask(__name__)
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.sywhzma.mongodb.net/?retryWrites=true&w=majority')
@@ -7,32 +7,25 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    count = len(bucket_list) + 1
+@app.route("/guestbooks", methods=["POST"])
+def guestbooks_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    
     doc = {
-            'num': count,  #버킷 등록 시, db에서 특정 버킷을 찾기 위해 'num' 이라는 고유 값 부여
-            'bucket' :bucket_receive,
-            'done' : 0   #'done' key값을 추가 해 각 버킷의 완료 상태 구분(0 = 미완료, 1 = 완료)
+        'name': name_receive,
+        'comment': comment_receive
     }
-    db.bucket.insert_one(doc)
+    db.fanlee.insert_one(doc) 
+
     return jsonify({'msg': '저장 완료!'})
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-    return jsonify({'msg': '버킷 완료!'})
- 
-    
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': bucket_list})
+@app.route("/guestbooks", methods=["GET"])
+def guestbooks_get():
+    all_comments = list(db.fanlee.find({},{'_id':False}))
+    return jsonify({'result': all_comments})
 
 if __name__ == '__main__':
-    app.run()
+   app.run('0.0.0.0', port=5001, debug=True)
